527c899e88687e9a5b6705cc57430f145bbf150d
shadow578 <52449218+shadow578@users.noreply.github.com>
2023-02-27 15:25:25 +0100

    cleanup `adc` driver

--- Marlin/lib/h32_core/drivers/adc/adc.cpp
+++ Marlin/lib/h32_core/drivers/adc/adc.cpp
@@ -1,310 +1,278 @@
-#define _ADC_HAL_C_
 #include "adc.h"
 #include "../board/board_gpio.h"
-#include "../MarlinHAL.h"
 
+//
+// ADC1 device
+//
 adc_dev adc1 = {
 	.HAL_AdcDmaIrqFlag = 0,
 	.HAL_adc_results = {0},
 
 	.regs = M4_ADC1,
 	.PeriphClock = PWC_FCG3_PERIPH_ADC1,
-	.Channel = ADC1_SA_CHANNEL /*|ADC1_SB_CHANNEL*/,
+	.Channel = ADC1_SA_CHANNEL,
 
 	.DMARegs = M4_DMA1,
 	.DMAPeriphClock = PWC_FCG0_PERIPH_DMA1,
 	.DMAChannel = DmaCh3,
 	.DMAenSrc = EVT_ADC1_EOCA,
 };
-/** ADC1 device. */
 adc_dev *ADC1 = &adc1;
 
-extern void adc_main(void)
+uint16_t adc_read(adc_dev *dev, uint8_t channel)
 {
-	/* Check ADC1 SA. */
-	if (ADC1->HAL_AdcDmaIrqFlag & ADC1_SA_DMA_IRQ_BIT)
-	{
-		ADC1->HAL_AdcDmaIrqFlag &= ~ADC1_SA_DMA_IRQ_BIT;
-		printf("\nADC1={%u,%u,%u,%u,%u,%u,%u}\r\n", ADC1->HAL_adc_results[0], ADC1->HAL_adc_results[1], ADC1->HAL_adc_results[2], ADC1->HAL_adc_results[3], ADC1->HAL_adc_results[4], ADC1->HAL_adc_results[5], ADC1->HAL_adc_results[6]);
-		// Do something with m_au16Adc1SaValue.
-		// ADC_StartConvert(ADC1->regs);
-	}
-}
-
-uint16_t  adc_read(adc_dev *dev, uint8_t channel)
-{
-	static uint16_t adc_results = 0;
-	do
+	// wait for adc result
+	while (true)
 	{
 		if (dev->HAL_AdcDmaIrqFlag & ADC1_SA_DMA_IRQ_BIT)
-			break;
-		uint8_t cnt3 = 0;
-		if (++cnt3 >= 100)
 		{
-			cnt3 = 0;
-			return (adc_results);
+			break;
 		}
-		MarlinHAL::watchdog_refresh();
-	} while (1);
-	dev->HAL_AdcDmaIrqFlag &= ~ADC1_SA_DMA_IRQ_BIT;
-	if (dev->HAL_adc_results[channel] == 0)
-	{
-		return (adc_results);
+
+		uint8_t c = 0;
+		if (++c >= 100)
+		{
+			c = 0;
+			return 0;
+		}
+
+		WDT_RefreshCounter();
 	}
-	adc_results = (dev->HAL_adc_results[channel]);
-	return (uint16_t)(dev->HAL_adc_results[channel]);
+
+	// read result and clear irq flag
+	dev->HAL_AdcDmaIrqFlag &= ~ADC1_SA_DMA_IRQ_BIT;
+	return dev->HAL_adc_results[channel];
 }
 
-static void  adc_setclk(void)
+void adc_setCLK()
 {
 #ifdef use_default_clk
 	CLK_SetPeriClkSource(ClkPeriSrcPclk);
 #else
-	stc_clk_xtal_cfg_t stcXtalCfg;
-	stc_clk_upll_cfg_t stcUpllCfg;
 
-	MEM_ZERO_STRUCT(stcXtalCfg);
-	MEM_ZERO_STRUCT(stcUpllCfg);
-
-	/* Use XTAL as UPLL source. */
-	stcXtalCfg.enFastStartup = Enable;
-	stcXtalCfg.enMode = ClkXtalModeOsc;
-	stcXtalCfg.enDrv = ClkXtalLowDrv;
-	CLK_XtalConfig(&stcXtalCfg);
+	// Use XTAL as UPLL source
+	stc_clk_xtal_cfg_t xtalConf = {
+		.enFastStartup = Enable,
+		.enMode = ClkXtalModeOsc,
+		.enDrv = ClkXtalLowDrv,
+	};
+	CLK_XtalConfig(&xtalConf);
 	CLK_XtalCmd(Enable);
 
-	/* Set UPLL out 240MHz. */
-	stcUpllCfg.pllmDiv = 2u;
-	/* upll = 8M(XTAL) / pllmDiv * plln */
-	stcUpllCfg.plln = 60u;
-	stcUpllCfg.PllpDiv = 16u;
-	stcUpllCfg.PllqDiv = 16u;
-	stcUpllCfg.PllrDiv = 16u;
+	stc_clk_upll_cfg_t upllConf = {
+		// upll = 8M(XTAL) / pllmDiv * plln
+		.PllpDiv = 16u,
+		.PllqDiv = 16u,
+		.PllrDiv = 16u,
+		.plln = 60u,
+
+		// Set UPLL out 240MHz
+		.pllmDiv = 2u,
+	};
 	CLK_SetPllSource(ClkPllSrcXTAL);
-	CLK_UpllConfig(&stcUpllCfg);
+	CLK_UpllConfig(&upllConf);
 	CLK_UpllCmd(Enable);
 	CLK_SetPeriClkSource(ClkPeriSrcUpllr);
 #endif
 }
 
-static void  adc_initconfig(adc_dev *dev)
+void adc_initConfig(adc_dev *dev)
 {
-	stc_adc_init_t stcAdcInit;
-	MEM_ZERO_STRUCT(stcAdcInit);
-	stcAdcInit.enResolution = AdcResolution_10Bit;
-	stcAdcInit.enDataAlign = AdcDataAlign_Right;
-	stcAdcInit.enAutoClear = AdcClren_Enable;
-	stcAdcInit.enScanMode = AdcMode_SAContinuous;
-	stcAdcInit.enRschsel = AdcRschsel_Restart;
-	/* 1. Enable ADC1. */
+	// enable adc1
 	PWC_Fcg3PeriphClockCmd(dev->PeriphClock, Enable);
-	/* 2. Initialize ADC1. */
-	ADC_Init(dev->regs, &stcAdcInit);
+
+	// initialize adc1
+	stc_adc_init_t adcConf = {
+		.enResolution = AdcResolution_10Bit,
+		.enDataAlign = AdcDataAlign_Right,
+		.enAutoClear = AdcClren_Enable,
+		.enScanMode = AdcMode_SAContinuous,
+		.enRschsel = AdcRschsel_Restart,
+	};
+	ADC_Init(dev->regs, &adcConf);
 }
 
-static void adc_SetPinMode(uint8_t u8AdcPin, en_pin_mode_t enMode)
+void adc_setPinMode(uint8_t adcPin, en_pin_mode_t mode)
 {
-	bool bFlag = true;
-	stc_port_init_t stcPortInit;
-	uint8_t PIN;
-
-	MEM_ZERO_STRUCT(stcPortInit);
-	stcPortInit.enPinMode = enMode;
-	stcPortInit.enPullUp = Disable;
-
-	switch (u8AdcPin)
+	// translate adc input to pin
+	uint8_t pin;
+	switch (adcPin)
 	{
 	case ADC1_IN0:
-		PIN = PA0;
+		pin = PA0;
 		break;
-
 	case ADC1_IN1:
-		PIN = PA1;
+		pin = PA1;
 		break;
-
 	case ADC1_IN2:
-		PIN = PA2;
+		pin = PA2;
 		break;
-
 	case ADC1_IN3:
-		PIN = PA3;
+		pin = PA3;
 		break;
-
 	case ADC12_IN4:
-		PIN = PA4;
+		pin = PA4;
 		break;
-
 	case ADC12_IN5:
-		PIN = PA5;
+		pin = PA5;
 		break;
-
 	case ADC12_IN6:
-		PIN = PA6;
+		pin = PA6;
 		break;
-
 	case ADC12_IN7:
-		PIN = PA7;
+		pin = PA7;
 		break;
-
 	case ADC12_IN8:
-		PIN = PB0;
+		pin = PB0;
 		break;
-
 	case ADC12_IN9:
-		PIN = PB1;
+		pin = PB1;
 		break;
-
 	case ADC12_IN10:
-		PIN = PC0;
+		pin = PC0;
 		break;
-
 	case ADC12_IN11:
-		PIN = PC1;
+		pin = PC1;
 		break;
-
 	case ADC1_IN12:
-		PIN = PC2;
+		pin = PC2;
 		break;
-
 	case ADC1_IN13:
-		PIN = PC3;
+		pin = PC3;
 		break;
-
 	case ADC1_IN14:
-		PIN = PC4;
+		pin = PC4;
 		break;
-
 	case ADC1_IN15:
-		PIN = PC5;
+		pin = PC5;
 		break;
-
 	default:
-		bFlag = false;
-		break;
+		return;
 	}
 
-	if (true == bFlag)
-	{
-		PORT_InitMapp(PIN, &stcPortInit);
-	}
+	// set pin mode
+	stc_port_init_t portConf = {
+		.enPinMode = mode,
+		.enPullUp = Disable,
+	};
+	PORT_InitMapp(pin, &portConf);
 }
 
-static void  adc_SetChannelPinMode(const M4_ADC_TypeDef *ADCx, uint32_t u32Channel, en_pin_mode_t enMode)
+void adc_setChannelPinMode(const M4_ADC_TypeDef *ADCx, uint32_t channel, en_pin_mode_t mode)
 {
-	uint8_t u8ChIndex;
-	uint8_t u8ChOffset = 0u;
-
+	// get channel offset and mask
+	uint8_t channelOffset = 0u;
 	if (M4_ADC1 == ADCx)
 	{
-		u32Channel &= ADC1_PIN_MASK_ALL;
+		channel &= ADC1_PIN_MASK_ALL;
 	}
 	else
 	{
-		u32Channel &= ADC2_PIN_MASK_ALL;
-		u8ChOffset = 4u;
+		channel &= ADC2_PIN_MASK_ALL;
+		channelOffset = 4u;
 	}
 
-	u8ChIndex = 0u;
-	while (0u != u32Channel)
+	// set pin mode of all pins in the channel
+	for (uint8_t i = 0u; channel != 0u; i++)
 	{
-		if (u32Channel & 0x1ul)
+		if (channel & 0x1ul)
 		{
-			adc_SetPinMode((u8ChIndex + u8ChOffset), enMode);
+			adc_setPinMode((channelOffset + i), mode);
 		}
 
-		u32Channel >>= 1u;
-		u8ChIndex++;
+		channel >>= 1u;
 	}
 }
 
-static void  adc_channelchonfig(adc_dev *dev, en_pin_mode_t enMode) /* onay ADC_SEQ_A and ADC1*/
+void adc_channelConfig(adc_dev *dev, en_pin_mode_t mode)
 {
-	stc_adc_ch_cfg_t stcChCfg;
-#define ADC1_SA_CHANNEL_SAMPLE_TIME {50, 50}
-	uint8_t au8Adc1SaSampTime[ADC1_SA_CHANNEL_COUNT] = ADC1_SA_CHANNEL_SAMPLE_TIME;
-	MEM_ZERO_STRUCT(stcChCfg);
+	static uint8_t samplingTimes[ADC1_SA_CHANNEL_COUNT] = {50, 50};
 
-	/**************************** Add ADC1 channels ****************************/
-	/* 1. Set the ADC pin to analog mode. */
-	adc_SetChannelPinMode(dev->regs, dev->Channel, enMode);
+	// set pin mode to analog
+	adc_setChannelPinMode(dev->regs, dev->Channel, mode);
 
-	stcChCfg.u32Channel = dev->Channel;
-	stcChCfg.u8Sequence = ADC_SEQ_A;
-	stcChCfg.pu8SampTime = au8Adc1SaSampTime;
-	/* 2. Add ADC channel. */
-	ADC_AddAdcChannel(dev->regs, &stcChCfg);
+	// init adc channel
+	stc_adc_ch_cfg_t adcChannelConf = {
+		.u32Channel = dev->Channel,
+		.u8Sequence = ADC_SEQ_A,
+		.pu8SampTime = samplingTimes,
+	};
+	ADC_AddAdcChannel(dev->regs, &adcChannelConf);
 }
-static void  adc_TriggerConfig(adc_dev *dev, uint32_t u32Fcg0Periph)
+
+void adc_triggerConfig(adc_dev *dev, uint32_t fcg0Periph)
 {
-	stc_adc_trg_cfg_t stcTrgCfg;
-	MEM_ZERO_STRUCT(stcTrgCfg);
-	PWC_Fcg0PeriphClockCmd(u32Fcg0Periph, Enable);
-	/* Select EVT_TMR02_GCMA as ADC1 sequence A trigger source. */
+	PWC_Fcg0PeriphClockCmd(fcg0Periph, Enable);
+
 	/*
-	 stcTrgCfg.u8Sequence = ADC_SEQ_A;
-	stcTrgCfg.enTrgSel   = AdcTrgsel_TRGX0;
-	stcTrgCfg.enInTrg0   = EVT_TMR02_GCMA;
-	ADC_ConfigTriggerSrc(dev->regs, &stcTrgCfg);
+	// select EVT_TMR02_GCMA as ADC1 trigger source
+	stc_adc_trg_cfg_t triggerConf = {
+		.u8Sequence = ADC_SEQ_A,
+		.enTrgSel = AdcTrgsel_TRGX0,
+		.enInTrg0 = EVT_TMR02_GCMA,
+	};
+	ADC_ConfigTriggerSrc(dev->regs, &triggerConf);
 	ADC_TriggerSrcCmd(dev->regs, ADC_SEQ_A, Enable);
 	*/
-	/* ADC1 sequence A will be started by software. */
+
+	// ADC1 is always triggered by software
 	ADC_TriggerSrcCmd(dev->regs, ADC_SEQ_A, Disable);
 }
 
-static void  adc_DMAInitConfig(adc_dev *dev)
+void adc_dmaInitConfig(adc_dev *dev)
 {
-	stc_dma_config_t stcDmaCfg;
+	// setup dma config
+	stc_dma_config_t dmaConf;
+	dmaConf.u16BlockSize = ADC1_CH_COUNT;
+	dmaConf.u16TransferCnt = 0u;
+	dmaConf.u32SrcAddr = (uint32_t)(&dev->regs->DR0);
+	dmaConf.u32DesAddr = (uint32_t)(&dev->HAL_adc_results[0]);
+	dmaConf.u16DesRptSize = ADC1_CH_COUNT;
+	dmaConf.u16SrcRptSize = ADC1_CH_COUNT;
+	dmaConf.u32DmaLlp = 0u;
+	dmaConf.stcSrcNseqCfg.u16Cnt = 0u;
+	dmaConf.stcSrcNseqCfg.u32Offset = 0u;
+	dmaConf.stcDesNseqCfg.u16Cnt = 0u;
+	dmaConf.stcDesNseqCfg.u32Offset = 0u;
+	dmaConf.stcDmaChCfg.enSrcInc = AddressIncrease;
+	dmaConf.stcDmaChCfg.enDesInc = AddressIncrease;
+	dmaConf.stcDmaChCfg.enSrcRptEn = Enable;
+	dmaConf.stcDmaChCfg.enDesRptEn = Enable;
+	dmaConf.stcDmaChCfg.enSrcNseqEn = Disable;
+	dmaConf.stcDmaChCfg.enDesNseqEn = Disable;
+	dmaConf.stcDmaChCfg.enTrnWidth = Dma16Bit;
+	dmaConf.stcDmaChCfg.enLlpEn = Disable;
 
-	MEM_ZERO_STRUCT(stcDmaCfg);
-
-	stcDmaCfg.u16BlockSize = ADC1_CH_COUNT;
-	stcDmaCfg.u16TransferCnt = 0u;
-	stcDmaCfg.u32SrcAddr = (uint32_t)(&dev->regs->DR0);
-	stcDmaCfg.u32DesAddr = (uint32_t)(&dev->HAL_adc_results[0]);
-	stcDmaCfg.u16DesRptSize = ADC1_CH_COUNT;
-	stcDmaCfg.u16SrcRptSize = ADC1_CH_COUNT;
-	stcDmaCfg.u32DmaLlp = 0u;
-	stcDmaCfg.stcSrcNseqCfg.u16Cnt = 0u;
-	stcDmaCfg.stcSrcNseqCfg.u32Offset = 0u;
-	stcDmaCfg.stcDesNseqCfg.u16Cnt = 0u;
-	stcDmaCfg.stcDesNseqCfg.u32Offset = 0u;
-	stcDmaCfg.stcDmaChCfg.enSrcInc = AddressIncrease;
-	stcDmaCfg.stcDmaChCfg.enDesInc = AddressIncrease;
-	stcDmaCfg.stcDmaChCfg.enSrcRptEn = Enable;
-	stcDmaCfg.stcDmaChCfg.enDesRptEn = Enable;
-	stcDmaCfg.stcDmaChCfg.enSrcNseqEn = Disable;
-	stcDmaCfg.stcDmaChCfg.enDesNseqEn = Disable;
-	stcDmaCfg.stcDmaChCfg.enTrnWidth = Dma16Bit;
-	stcDmaCfg.stcDmaChCfg.enLlpEn = Disable;
-	/* Enable DMA interrupt. */
-	stcDmaCfg.stcDmaChCfg.enIntEn = Enable;
+	// enable DMA interrupt
+	dmaConf.stcDmaChCfg.enIntEn = Enable;
 
 	PWC_Fcg0PeriphClockCmd(dev->DMAPeriphClock, Enable);
-	DMA_InitChannel(dev->DMARegs, dev->DMAChannel, &stcDmaCfg);
+	DMA_InitChannel(dev->DMARegs, dev->DMAChannel, &dmaConf);
+
 	DMA_Cmd(dev->DMARegs, Enable);
 	DMA_ChannelCmd(dev->DMARegs, dev->DMAChannel, Enable);
 	DMA_ClearIrqFlag(dev->DMARegs, dev->DMAChannel, TrnCpltIrq);
 	DMA_ClearIrqFlag(dev->DMARegs, dev->DMAChannel, BlkTrnCpltIrq);
-	/* AOS must be enabled to use DMA */
-	/* AOS enabled at first. */
-	/* If you have enabled AOS before, then the following statement is not needed. */
+
+	// AOS must be enabled to use DMA
+	// AOS enabled at first
+	// If you have enabled AOS before, then the following statement is not needed
 	PWC_Fcg0PeriphClockCmd(PWC_FCG0_PERIPH_PTDIS, Enable);
 	DMA_SetTriggerSrc(dev->DMARegs, dev->DMAChannel, dev->DMAenSrc);
 }
 
-static void  adc_DmaIrqRegister(stc_irq_regi_conf_t *pstcCfg, uint32_t u32Priority)
+void adc_dmaRegisterIRQ(stc_irq_regi_conf_t *pstcCfg, uint32_t priority)
 {
-	int16_t s16Vnum = pstcCfg->enIRQn;
-
-	if (((s16Vnum >= Int000_IRQn) && (s16Vnum <= Int031_IRQn)) ||
-		((s16Vnum >= Int038_IRQn) && (s16Vnum <= Int043_IRQn)))
+	int16_t irqNum = pstcCfg->enIRQn;
+	if (((irqNum >= Int000_IRQn) && (irqNum <= Int031_IRQn)) ||
+		((irqNum >= Int038_IRQn) && (irqNum <= Int043_IRQn)))
 	{
-		if (Ok != enIrqRegistration(pstcCfg))
+		if (enIrqRegistration(pstcCfg) != Ok)
 		{
 			return;
 		}
 	}
-	else if (Int129_IRQn == s16Vnum)
+	else if (irqNum == Int129_IRQn)
 	{
 		enShareIrqEnable(pstcCfg->enIntSrc);
 	}
@@ -312,55 +280,49 @@ static void  adc_DmaIrqRegister(stc_irq_regi_conf_t *pstcCfg, uint32_t u32Priori
 	{
 		return;
 	}
+
 	NVIC_ClearPendingIRQ(pstcCfg->enIRQn);
-	NVIC_SetPriority(pstcCfg->enIRQn, u32Priority);
+	NVIC_SetPriority(pstcCfg->enIRQn, priority);
 	NVIC_EnableIRQ(pstcCfg->enIRQn);
 }
 
 /**
- *******************************************************************************
- ** \brief DMA IRQ callbacks.
- **
- ******************************************************************************/
+ * ADC DMA IRQ handler 
+ */
 void Dma1Btc3_IrqHandler(void)
 {
 	// DMA_ClearIrqFlag(ADC1->DMARegs, ADC1->DMAChannel, BlkTrnCpltIrq);
 	ADC1->HAL_AdcDmaIrqFlag |= ADC1_SA_DMA_IRQ_BIT;
 }
 
-static void  adc_DMAIrqConfig(void)
+void adc_dmaIRQConfig(void)
 {
-	stc_irq_regi_conf_t stcAdcIrqCfg;
-	MEM_ZERO_STRUCT(stcAdcIrqCfg);
-	stcAdcIrqCfg.enIntSrc = INT_DMA1_BTC3;
-	stcAdcIrqCfg.enIRQn = Int029_IRQn;
-	stcAdcIrqCfg.pfnCallback = &Dma1Btc3_IrqHandler;
-	adc_DmaIrqRegister(&stcAdcIrqCfg, DDL_IRQ_PRIORITY_DEFAULT);
+	stc_irq_regi_conf_t stcAdcIrqCfg = {
+		.enIntSrc = INT_DMA1_BTC3,
+		.enIRQn = Int029_IRQn,
+		.pfnCallback = &Dma1Btc3_IrqHandler,
+	};
+	adc_dmaRegisterIRQ(&stcAdcIrqCfg, DDL_IRQ_PRIORITY_DEFAULT);
 }
 
-static void  adc_default_config(adc_dev *dev)
+void adc_setDefaultConfig(adc_dev *dev)
 {
-	adc_initconfig(dev);
-	adc_channelchonfig(dev, Pin_Mode_Ana);
-	adc_TriggerConfig(dev, PWC_FCG0_PERIPH_PTDIS);
-	/* Config DMA. */
-	adc_DMAInitConfig(dev);
-	adc_DMAIrqConfig();
+	// init and config adc and channels
+	adc_initConfig(dev);
+	adc_channelConfig(dev, Pin_Mode_Ana);
+	adc_triggerConfig(dev, PWC_FCG0_PERIPH_PTDIS);
+
+	// init and config DMA
+	adc_dmaInitConfig(dev);
+	adc_dmaIRQConfig();
 	ADC_StartConvert(dev->regs);
 }
 
-static void adc_foreach(void (*fn)(adc_dev *))
+void adc_init(void)
 {
-	fn(ADC1);
-}
+	// set ADC clock (default is MRC @ 8MHz)
+	adc_setCLK();
 
-extern void  adc_init(void)
-{
-	/* Default clock is MRC(8MHz). */
-	adc_setclk();
-	/* Config ADCs. */
-	adc_foreach(adc_default_config);
+	// configure ADC
+	adc_setDefaultConfig(ADC1);
 }
-
-#undef _ADC_HAL_C_
-/************end of file********************/
--- Marlin/lib/h32_core/drivers/adc/adc.h
+++ Marlin/lib/h32_core/drivers/adc/adc.h
@@ -7,15 +7,23 @@
 extern "C"
 {
 #endif
+#define ADC_CHANNEL_COUNT 2u
+#define ADC1_SA_CHANNEL (ADC1_CH14 | ADC1_CH15)
+#define ADC1_SA_CHANNEL_COUNT (ADC_CHANNEL_COUNT)
 
-	/** ADC device type. */
+// ADC irq flag bit mask
+#define ADC1_SA_DMA_IRQ_BIT (1ul << 0u)
+
+	//
+	// ADC device definition
+	//
 	typedef struct adc_dev
 	{
 		__IO uint32_t HAL_AdcDmaIrqFlag;
 		__IO uint16_t HAL_adc_results[ADC1_CH_COUNT];
 
-		M4_ADC_TypeDef *regs;	   /**< Register map */
-		__IO uint32_t PeriphClock; /**< clock information */
+		M4_ADC_TypeDef *regs; 
+		__IO uint32_t PeriphClock;
 		__IO uint32_t Channel;
 
 		M4_DMA_TypeDef *DMARegs;
@@ -23,31 +31,12 @@ extern "C"
 		__IO uint8_t DMAChannel;
 		__IO en_event_src_t DMAenSrc;
 	} adc_dev;
+
 	extern adc_dev adc1;
 	extern struct adc_dev *ADC1;
 
-/********************************************************/
-/********************************************************/
-// ADC1 channel definition ch0~ch3.
-#define ADC_CHANNEL_COUNT 2u
-#define ADC1_SA_CHANNEL (ADC1_CH14 | ADC1_CH15)
-#define ADC1_SA_CHANNEL_COUNT (ADC_CHANNEL_COUNT)
-// ADC1 channel sampling time.      ADC1_CH0  ADC1_CH1 ADC1_CH2
-/********************************************************/
-/********************************************************/
-// #define ADC1_SB_CHANNEL             (ADC1_CH4|ADC1_CH5|ADC1_CH6)
-// #define ADC1_SB_CHANNEL_COUNT       (3u)
-// #define ADC1_SB_CHANNEL_SAMPLE_TIME { 0x50,0x60,0x45 }
-/********************************************************/
-/********************************************************/
-
-/* ADC interrupt flag bit mask definition. */
-#define ADC1_SA_DMA_IRQ_BIT (1ul << 0u)
-
-	extern void adc_init(void);
-	extern void adc_main(void);
+	void adc_init();
 	uint16_t adc_read(adc_dev *dev, uint8_t channel);
-
 #ifdef __cplusplus
 }
 #endif
